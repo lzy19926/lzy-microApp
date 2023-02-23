3f3c848aa67f21b7428ebc41cc00c56e86699c49
阳九 <871921282@qq.com>
2023-02-23 14:28:52 +0800

    替换依赖,从node_modules中获取依赖

--- lib/index.ts
+++ lib/index.ts
@@ -1,13 +1,14 @@
-import { MicroAppElement } from './microApp_element'
-import logger from './logger';
+import { MicroAppElement } from './core//microApp_element'
+import logger from './core/logger';
 
 // 也就是start方法
 export function start({
     showLoggerInfo = true
 }) {
-
     // 初始化logger
     logger.showLoggerInfo = showLoggerInfo
     // 启动,初始化customELE
     customElements.define('micro-app-element', MicroAppElement);
 }
+
+export default { start }
--- out/index.js
+++ out/index.js
@@ -4,8 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.start = void 0;
-const microApp_element_1 = require("./microApp_element");
-const logger_1 = __importDefault(require("./logger"));
+const microApp_element_1 = require("./core//microApp_element");
+const logger_1 = __importDefault(require("./core/logger"));
 // 也就是start方法
 function start({ showLoggerInfo = true }) {
     // 初始化logger
@@ -14,3 +14,4 @@ function start({ showLoggerInfo = true }) {
     customElements.define('micro-app-element', microApp_element_1.MicroAppElement);
 }
 exports.start = start;
+exports.default = { start };
